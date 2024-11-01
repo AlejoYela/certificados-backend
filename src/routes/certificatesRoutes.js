244617010240a57b6bc4b8915f21bb62c1213609
Alejandro Yela <andres.alejo.yela@gmail.com>
2024-10-31 22:15:42 -0500

    New get route

--- src/routes/certificatesRoutes.js
+++ src/routes/certificatesRoutes.js
@@ -27,6 +27,34 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.get("/validate/:certificateId", async (req, res) => {
+  try {
+    // Busca en todos los usuarios un certificado específico en su array de `certificates`
+    const user = await Person.findOne({
+      "certificates.certificate": req.params.certificateId,
+    });
+
+    // Si no se encuentra el usuario o el certificado, envía un error 404
+    if (!user) {
+      return res.status(404).json({ message: "Certificado no encontrado" });
+    }
+
+    // Encuentra el certificado específico dentro del array de certificados del usuario
+    const certificate = user.certificates.find(
+      (cert) => cert.certificate === req.params.certificateId
+    );
+
+    // Devuelve la información del usuario y solo el certificado que coincide
+    res.json({
+      name: user.name,
+      id: user.id,
+      certificate, // solo el certificado específico
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener el certificado", error });
+  }
+});
+
 // Ruta para agregar un certificado a una persona
 router.post("/add", async (req, res) => {
   const { name, id, courseTitle } = req.body; // Incluye id aquí
